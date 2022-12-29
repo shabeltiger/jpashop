5684874bfba5b9017f18fa84684c43bcbfdb0d72
ksh <shabeltiger@gmail.com>
2022-12-29 12:01:21 +0900

    12시에 테스트 커밋.

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        memberRepository.save(member);
+    }
+}
+
